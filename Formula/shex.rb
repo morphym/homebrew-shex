aa56a48f69b1780741b88e94c0f0aaa6a3a5a6a8
Amon <amon@pawit.co>
2026-09-17 15:16:57 +0530

    Update shex to 2.0.3

--- Formula/shex.rb
+++ Formula/shex.rb
@@ -1,26 +1,26 @@
 class Shex < Formula
   desc "OPAQUE-authenticated, end-to-end encrypted Redis remote shell"
   homepage "https://github.com/morphym/shex"
-  version "2.0.2"
+  version "2.0.3"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/morphym/shex/releases/download/v2.0.2/shex-2.0.2-aarch64-apple-darwin.tar.gz"
-      sha256 "1ad9b91138f77118462f127dd81fb8e00aaf47d2262d25305b0cdb190476b67b"
+      url "https://github.com/morphym/shex/releases/download/v2.0.3/shex-2.0.3-aarch64-apple-darwin.tar.gz"
+      sha256 "e8a4be35dceeec0aaf0ec349aee67559de191f7ba81fffa2167d990050baa77c"
     else
-      url "https://github.com/morphym/shex/releases/download/v2.0.2/shex-2.0.2-x86_64-apple-darwin.tar.gz"
-      sha256 "c91ace43679de52cf78e36b895cb4b543b8beb334c7726ce48a47b3852f4b4c3"
+      url "https://github.com/morphym/shex/releases/download/v2.0.3/shex-2.0.3-x86_64-apple-darwin.tar.gz"
+      sha256 "0d7c5873944d0666adff8674efbe010fe5671161a2b0e0001e7ed0992567cfff"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/morphym/shex/releases/download/v2.0.2/shex-2.0.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "cb86cff35e48f32342627aa02a961b19f761b763a79cc10592a3d351af19ebe0"
+      url "https://github.com/morphym/shex/releases/download/v2.0.3/shex-2.0.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "518b617b6fda02909c72c3661e3a33efa839ca14e383b879292003f5bf00af45"
     else
-      url "https://github.com/morphym/shex/releases/download/v2.0.2/shex-2.0.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1b98462467d7f6fa7ee77c7c735cebe62a4a75bbf301ac6d44912e82e6899f29"
+      url "https://github.com/morphym/shex/releases/download/v2.0.3/shex-2.0.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "eca14ef5931335959c75c508c98eddad997562e360c09a8fd96d87a2a4905239"
     end
   end
 
